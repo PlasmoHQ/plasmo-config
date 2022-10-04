10dd7bba1f0232c71b070ed394f4ecb7abe46866
Louis <6723574+louisgv@users.noreply.github.com>
2022-10-03 22:45:42 -0400

    feat: centralize process tsd

--- ts/process.d.ts
+++ ts/process.d.ts
@@ -1,5 +1,22 @@
 declare namespace NodeJS {
   interface ProcessEnv {
+    // Platform env
     PLASMO_ITERO_ENDPOINT?: string
+
+    // Framework env
+    /**
+     * Relative path to the source-code directory from project root
+     */
+    PLASMO_SRC_PATH?: string
+
+    /**
+     * this should point to the "source-code directory"
+     */
+    PLASMO_SRC_DIR?: string
+
+    // Generic env
+    APP_VERSION?: string
+    VERBOSE?: "true" | "false"
+    NODE_ENV?: "development" | "production" | "test"
   }
 }
