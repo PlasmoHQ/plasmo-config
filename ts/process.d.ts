1f71f3d85bdb5ca07644fbe5fdec982b35dee700
Louis <6723574+louisgv@users.noreply.github.com>
2022-11-20 14:00:14 -0400

    Adding runner config

--- ts/process.d.ts
+++ ts/process.d.ts
@@ -4,6 +4,8 @@ declare namespace NodeJS {
     PLASMO_ITERO_ENDPOINT?: string
 
     // Framework env
+    PLASMO_FRAMEWORK_INTERNAL_WATCHER_STARTED?: "true"
+
     /**
      * Absolute path to the project directory
      */
