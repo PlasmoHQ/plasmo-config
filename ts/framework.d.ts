f796914cb96ee7b8dd9c396fc478880569bfe906
L❤️ ☮️ ✋ <6723574+louisgv@users.noreply.github.com>
2023-05-26 17:09:21 -0400

    chore: Add a flag for es target

--- ts/framework.d.ts
+++ ts/framework.d.ts
@@ -6,5 +6,18 @@ declare namespace NodeJS {
     __PLASMO_FRAMEWORK_INTERNAL_NO_MINIFY?: "true" | "false"
 
     __PLASMO_FRAMEWORK_INTERNAL_NO_CS_RELOAD?: "true" | "false"
+
+    __PLASMO_FRAMEWORK_INTERNAL_ES_TARGET?:
+      | "es3"
+      | "es5"
+      | "es2015"
+      | "es2016"
+      | "es2017"
+      | "es2018"
+      | "es2019"
+      | "es2020"
+      | "es2021"
+      | "es2022"
+      | "esnext"
   }
 }
